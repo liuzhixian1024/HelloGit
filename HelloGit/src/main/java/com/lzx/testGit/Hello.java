f9674e95c0cef552cbe7748e5dd6a85e9153df2e
json_glb <liuzhixian_glb@qq.com>
2021-01-04 15:42:51 +0800

    clone,再提交一哈

--- HelloGit/src/main/java/com/lzx/testGit/Hello.java
+++ HelloGit/src/main/java/com/lzx/testGit/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("hello, git");
         System.out.println("更新1");
         System.out.println("在线更新一下");
+        System.out.println("clone下的项目,再提交一哈!");
     }
 }
